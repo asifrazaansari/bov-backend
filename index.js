dce413755f3af95aa033606e7e02e72f2948ee7d
asifrazaansari <asifraza0231@gmail.com>
2024-11-27 08:30:47 +0530

    Added coming soon message

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/*', (req, res) => {
+    res.send('Coming Soon');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
